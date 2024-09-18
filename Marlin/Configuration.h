a873bef2a27b8f900eeb922525fff1f7cf7c0c7b
Solflame <38461095+Solflame@users.noreply.github.com>
2024-09-18 13:30:45 +1000

    Updated probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1605,7 +1605,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -45, -5, -1.2 }
+#define NOZZLE_TO_PROBE_OFFSET { -45, -10, -1.2 }
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
